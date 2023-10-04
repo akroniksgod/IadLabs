e1bf9499e69c786e4293c2f0a194738a790b47e7
tedeev <tedeev@ursalab.ru>
2023-10-04 21:52:10 +0500

    Исправления в ЛР1
    Вынесены даты в список, формируемый отдельной функцией;
    Исправлена проблема с графиком.

--- LW1 (Currency Rates)/main.py
+++ LW1 (Currency Rates)/main.py
@@ -1,38 +1,42 @@
+import matplotlib
 import matplotlib.pyplot as plt
 import requests
 import datetime
 from matplotlib.dates import DateFormatter
 
 
-def calc(col):
+def calc(col: list[dict]) -> None:
     ax = plt.figure().add_subplot(projection='3d')
-    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
     ax.set_xlabel("Дата")
     ax.set_ylabel("Курс доллара")
     ax.set_zlabel("Изменение")
+
+    ax.xaxis.set_major_locator(matplotlib.dates.AutoDateLocator())
+    ax.xaxis.set_major_formatter(matplotlib.dates.DateFormatter("%Y-%m-%d"))
 
     x = []
     y = []
     z = []
 
     for i in col:
-        x.append(datetime.datetime.strptime(i.get("date"), '%Y-%m-%d'))
+        x.append(i.get("date"))
         y.append(i.get("usd"))
         z.append(i.get("delta"))
 
-    ax.errorbar(x, y, z, 0.2)
+    x_dates = matplotlib.dates.date2num(x)
+    ax.plot(x_dates, y, z)
     plt.show()
 
 
-def get_calculated_delta_dict(col):
+def get_calculated_delta_dict(col: list[dict]) -> list[dict]:
     new_col = []
     next_item = None
     length = len(col)
+    key = "usd"
     for index, obj in enumerate(col):
         if index < (length - 1):
             next_item = col[index + 1]
 
-        key = "usd"
         prev_usd = obj[key]
         current_usd = next_item[key]
         new_col.append({**obj, "delta": (prev_usd - current_usd) / current_usd * 100})
@@ -40,33 +44,43 @@ def get_calculated_delta_dict(col):
     return new_col
 
 
-def get_dict():
+def get_dict(prepared_dates: list) -> list[dict]:
     col = []
-    days_count = 14
+    for current_date in prepared_dates:
+        prepared_link = 'https://www.cbr-xml-daily.ru/archive/{}/{}/{}/daily_json.js'
+
+        month = '0' + str(current_date.month) if current_date.month < 10 else current_date.month
+        day = '0' + str(current_date.day) if current_date.day < 10 else current_date.day
+
+        link = prepared_link.format(current_date.year, month, day)
+        current_request = requests.get(link)
+        current_parsed_request = current_request.json()
+        usd = current_parsed_request['Valute']['USD']['Value']
+
+        col.append({"date": current_date, "usd": usd})
+
+    return col
+
+
+def get_dates() -> list[datetime]:
+    count = 14
+    this_dates = []
     date = datetime.datetime.today()
-    for i in range(days_count):
+
+    for i in range(count):
         prev_date = date - datetime.timedelta(days=1)
         date = prev_date
 
         if prev_date.weekday() == 0 or prev_date.weekday() == 6:
             continue
 
-        prepared_link = 'https://www.cbr-xml-daily.ru/archive/{}/{}/{}/daily_json.js'
+        this_dates.append(prev_date.date())
 
-        month = '0' + str(prev_date.month) if prev_date.month < 10 else prev_date.month
-        day = '0' + str(prev_date.day) if prev_date.day < 10 else prev_date.day
-
-        link = prepared_link.format(prev_date.year, month, day)
-        current_request = requests.get(link)
-        current_parsed_request = current_request.json()
-        usd = current_parsed_request['Valute']['USD']['Value']
-
-        col.append({"date": str(prev_date.date()), "usd": usd})
-
-    return col
+    return this_dates
 
 
 if __name__ == '__main__':
-    base_collection = get_dict()
+    dates = get_dates()
+    base_collection = get_dict(dates)
     prepared_collection = get_calculated_delta_dict(base_collection)
     calc(prepared_collection)
